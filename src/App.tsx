46f210ae88646a0d8709d1cf746f9a9f6d28d1e4
nabin.shrestha <nabin.shrestha@admin01033s-MacBook-Air.local>
2022-04-22 17:19:03 +0545

    feat: add theme and global style

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,19 @@ import { Toaster } from 'react-hot-toast';
 
 import { AppProvider } from 'providers/app';
 import { Routes } from 'providers/Routes';
+import ThemeConfig from 'theme';
+import ScrollToTop from 'utils/scrollToTop';
+import GlobalStyle from 'theme/globalStyle';
 
 function App() {
   return (
     <AppProvider>
-      <Routes />
-      <Toaster position="top-center" reverseOrder={false} />
+      <ThemeConfig>
+        <ScrollToTop />
+        <GlobalStyle />
+        <Routes />
+        <Toaster position="top-right" reverseOrder={false} />
+      </ThemeConfig>
     </AppProvider>
   );
 }
